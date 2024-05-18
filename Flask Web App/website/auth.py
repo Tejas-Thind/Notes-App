3bddb66d7e4879b991bf2ea7f6e3c5b55c13d923
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-05-18 21:15:49 +0000

    made sign up page

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -2,9 +2,10 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
