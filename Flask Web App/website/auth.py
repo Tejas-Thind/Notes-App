819f3257129c16126c3e3bc4755c4eeb2ccbf402
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-05-29 06:28:17 +0000

    making the flash alerts

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -4,6 +4,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST']) # going to the url is a GET request, pressing submit button is a POST request
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -22,9 +24,8 @@ def sign_up():
            flash('Email must be greater than 4 characters.', category='error')
        elif len(firstName) < 2:
            flash('First name mnust be greater than 1 characters.', category='error')
-
        elif password1 != password2:
-           flash('Passwords dont\'t match.', category='error')
+           flash('Passwords don\'t match.', category='error')
        elif len(password1) < 7:
            flash('Password must be at least 7 characters.', category='error')
        else:
