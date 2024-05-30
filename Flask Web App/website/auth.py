03dd2e7a2905b2d58c27581394c025e38adf7b24
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-05-30 05:55:37 +0000

    fixed hashing issue

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -22,7 +22,7 @@ def logout():
 def sign_up():
     if request.method == 'POST':
        email = request.form.get('email')
-       firstName = request.form.get('firstName') 
+       first_name = request.form.get('firstName') 
        password1 = request.form.get('password1') 
        password2 = request.form.get('password2')
 
@@ -33,7 +33,7 @@ def sign_up():
 
        if len(email) < 4:
            flash('Email must be greater than 3 characters.', category='error')
-       elif len(firstName) < 2:
+       elif len(first_name) < 2:
            flash('First name must be greater than 1 character.', category='error')
        elif password1 != password2:
            flash('Passwords don\'t match.', category='error')
@@ -42,7 +42,7 @@ def sign_up():
        elif counter == 0:
            flash('Password must contain a number.', category='error')
        else:
-           new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+           new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))           
            db.session.add(new_user)
            db.session.commit()
            flash('Account created!', category='success')
