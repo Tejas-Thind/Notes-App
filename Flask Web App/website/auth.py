3319d2f9d4f7fdf0fe13facb4a0fc2272e1624a2
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-05-30 06:24:30 +0000

    redirected users when they are not logged in

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 # Hashing function is a one way function that does not have an inverse function
-#
 
 auth = Blueprint('auth', __name__)
 
@@ -19,6 +19,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
@@ -27,8 +29,10 @@ def login():
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -61,6 +65,7 @@ def sign_up():
            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))           
            db.session.add(new_user)
            db.session.commit()
+           login_user(user, remember=True)
            flash('Account created!', category='success')
            return redirect(url_for('views.home'))
            
